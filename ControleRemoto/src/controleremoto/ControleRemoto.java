9cda4e938719b5f721382487e3e02a5d0d13a053
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-08-25 23:49:22 -0300

    Adicionando projeto
    Idealização de como seria um controle remoto com java e aprendendo o conceito de encapsulamento.

---
+++ ControleRemoto/src/controleremoto/ControleRemoto.java
@@ -0,0 +1,13 @@
+package controleremoto;
+public class ControleRemoto {
+   
+    public static void main(String[] args) {
+       ControladorClasse c = new ControladorClasse();
+       c.ligar();    
+       c.maisVolume();
+       c.play();
+       c.abrirMenu();
+       c.fecharMenu();
+    }
+    
+}
